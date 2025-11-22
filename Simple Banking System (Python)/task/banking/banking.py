c81fb5303a598e1ce6eaad68db98d67894e44baf
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-22 21:19:24 +0100

    WiP on menu

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -1,1 +1,30 @@
-# Write your code here
+"""Simple Banking System"""
+
+
+def display_menu():
+    print('1. Create an account')
+    print('2. Log into account')
+    print('0. Exit')
+
+
+def handle_command(command):
+    match command:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 0:
+            pass
+        case _:
+            pass
+
+
+def main():
+    while True:
+        display_menu()
+        command = input()
+        handle_command(command)
+
+
+if __name__ == "__main__":
+    main()
